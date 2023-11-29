1063799dd617ef68b33cd59ff990edc959605f3a
pallu-53 <109040762+pallu-53@users.noreply.github.com>
2023-11-29 20:26:45 +0530

    First Program

---
+++ src/com/radical/test/Helloworld.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.radical.test;
+
+/**
+ * 
+ */
+public class Helloworld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		int a, b, c;
+		a = 5;
+		b = 4;
+		c=a+b;
+		System.out.println(c);
+
+	}
+
+}
